63fca2d1e5a6800d2c49d7cc95eeb8163bb9eee5
Tomoyuki Matsuda <toocheap@gmail.com>
2017-05-10 12:55:51 +0900

    To run properly on the different culture environment. Set Culture to "en-US" explicitly.

--- GenMSRCDocs.ps1
+++ GenMSRCDocs.ps1
@@ -7,12 +7,22 @@
 ## Install the MSRC PowerShell cmdlets, Run in an Admin PowerShell:
 ## Install-Module -Name MSRCSecurityUpdates -force
 
+
+
+## Added Tomoyuki Matsuda, toocheap [at] gmail.com 10 May 2017
+# To run properly on the different culture environment. Set Culture to "en-US" explicitly.
+$currentThread = [System.Threading.Thread]::CurrentThread
+$culture = [System.Globalization.CultureInfo]::CreateSpecificCulture("en-US")
+$currentThread.CurrentCulture = $culture
+$currentThread.CurrentUICulture = $culture
+##
+
 ## In a normal user PowerShell:
 Import-Module MSRCSecurityUpdates -Verbose:$false
-Set-MSRCApiKey -ApiKey "your-api-key" 
+Set-MSRCApiKey -ApiKey "183820dd115c415a8e9a4b1d048e9267" 
 $month = Get-Date -format M |% {$_.Split(' ')[0]} 
 $year = Get-Date -format yyyy
 $timeperiod = $year + '-' + $month
 $fname = 'MSRCSecurityUpdates' + $timeperiod + '.html'
 Get-MsrcCvrfDocument -ID $timeperiod | Get-MsrcSecurityBulletinHtml | Out-File $fname
-Invoke-Item $fname 
+Invoke-Item $fname
